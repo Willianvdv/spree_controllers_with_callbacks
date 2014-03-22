7997003e4465712169e4af9086c55bd6ea18049c
Willian van der Velde <willian@pythonheads.nl>
2014-03-22 13:39:24 +0100

    Products#show with callbacks

--- app/controllers/spree/products_controller_decorator.rb
+++ app/controllers/spree/products_controller_decorator.rb
@@ -11,4 +11,15 @@ Spree::ProductsController.class_eval do
 
   alias_method :index_without_callbacks, :index
   alias_method :index, :index_with_callbacks
+
+  def show_with_callbacks
+    r = nil
+    run_callbacks :index do
+      r = show_without_callbacks
+    end
+    r
+  end
+
+  alias_method :show_without_callbacks, :show
+  alias_method :show, :show_with_callbacks
 end
