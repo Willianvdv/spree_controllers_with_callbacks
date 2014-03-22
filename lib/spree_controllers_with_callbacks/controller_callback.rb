5ae371dd6bb902ba617ad862852db7b7f5fcb153
Willian van der Velde <willian@pythonheads.nl>
2014-03-22 14:37:12 +0100

    Some doc

--- lib/spree_controllers_with_callbacks/controller_callback.rb
+++ lib/spree_controllers_with_callbacks/controller_callback.rb
@@ -4,10 +4,13 @@ module ControllerCallback
   included do
     include ActiveSupport::Callbacks
 
+    # The callbacks you can register on
     define_callbacks :show
     define_callbacks :index
 
     [:show, :index].each do |action|
+      # Create a new method that calls the orinal method surrounded with
+      # the run_callbacks
       define_method "#{action}_with_callbacks" do
         action_with_callbacks action
       end
@@ -20,6 +23,7 @@ module ControllerCallback
   protected
 
   def action_with_callbacks action
+    # Run the orignal method and preserve its return value
     r = nil
     run_callbacks action do
       r = self.send("#{action}_without_callbacks")
