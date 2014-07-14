e0cb6d6c8e30d0e9b2b0c588e02970d2d284cc19
Willian van der Velde <willian@pythonheads.nl>
2014-07-14 13:51:53 +0200

    Require controllers

--- lib/spree_controllers_with_callbacks.rb
+++ lib/spree_controllers_with_callbacks.rb
@@ -1,3 +1,6 @@
 require 'spree_core'
 require 'spree_controllers_with_callbacks/engine'
 require 'spree_controllers_with_callbacks/controller_callback'
+
+require 'spree_controllers_with_callbacks/controllers/spree/taxons_controller_decorator'
+require 'spree_controllers_with_callbacks/controllers/spree/products_controller_decorator'
